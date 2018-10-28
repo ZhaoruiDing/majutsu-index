0d022437751034b7354b2dee6209b9ec2e3419fb
ZhaoruiDing <zhaorui1225@gmail.com>
2018-10-28 03:12:46 -0500

    add login

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Register} from "./Register"
-
+import {Login} from "./Login"
 export class Main extends React.Component {
   render() {
     return (
         <div>
-          <Register/>
+          {/*<Register/>*/}
+          <Login/>
         </div>
     );
 
