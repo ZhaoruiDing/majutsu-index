a59d3f7237941870a2b025dab856fe17a2a632d9
Zhaorui Ding <zhaorui1225@gmail.com>
2018-11-27 01:27:40 -0600

    try to search search function

--- src/components/Home.js
+++ src/components/Home.js
@@ -358,7 +358,7 @@ export class Home extends React.Component{
   loadSearchDateResAll = (month, year) => {
     const UserEmail = localStorage.getItem(TOKEN_KEY);
     $.ajax({
-      url: `${API_ROOT}/search/rec?UserEmail=${UserEmail}&year=${year}&month=${month}`,
+      url: `${API_ROOT}/search/date?UserEmail=${UserEmail}&year=${year}&month=${month}`,
       method: 'GET',
       // month: month,
       // year: year,
