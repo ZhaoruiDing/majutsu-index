4103d9cad0c670e166bde8e5b6d4a22f89e53ac4
ZhaoruiDing <zhaorui1225@gmail.com>
2018-11-28 01:26:26 -0600

    final version 8

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,12 +19,14 @@ export class Home extends React.Component{
     favAnimes: [],
     recommendAnimes: [],
     wishAnimes: [],
+    popularAnimes[],
     error:'',
     loadingAnimes: false,
     loadingFavAnimes: false,
     loadingRecommendAnimes: false,
     loadingWishAnimes: false,
-    curTab: "all" //all, fav, rec, wish
+    loadingPopularAnimes: false,
+    curTab: "all" //all, fav, rec, wish, popular
   }
 
   componentDidMount(){
@@ -80,6 +82,7 @@ export class Home extends React.Component{
                       loadFavoriteAnimes={this.loadFavoriteAnimes}
                       loadRecommendAnimes = {this.loadRecommendAnimes}
                       loadWishAnimes={this.loadWishAnimes}
+                      loadPopularAnimes={this.loadPopularAnimes}
                       curTab = "all"
                       history={this.props.history}/>;
     }
@@ -114,6 +117,7 @@ export class Home extends React.Component{
                       loadFavoriteAnimes={this.loadFavoriteAnimes}
                       loadRecommendAnimes = {this.loadRecommendAnimes}
                       loadWishAnimes={this.loadWishAnimes}
+                      loadPopularAnimes={this.loadPopularAnimes}
                       curTab = "fav"/>;
     }
     else {
@@ -156,6 +160,7 @@ export class Home extends React.Component{
                       loadFavoriteAnimes={this.loadFavoriteAnimes}
                       loadRecommendAnimes = {this.loadRecommendAnimes}
                       loadWishAnimes={this.loadWishAnimes}
+                      loadPopularAnimes={this.loadPopularAnimes}
                       curTab = "rec"/>;
     }
     else {
@@ -189,7 +194,41 @@ export class Home extends React.Component{
                       loadFavoriteAnimes={this.loadFavoriteAnimes}
                       loadRecommendAnimes = {this.loadRecommendAnimes}
                       loadWishAnimes={this.loadWishAnimes}
+                      loadPopularAnimes={this.loadPopularAnimes}
                       curTab = "wish"/>;
+    }
+    else {
+      return null;
+    }
+  }
+
+  getGalleryPanelContentPopularAnimes = () => {
+    //this.setState({curTab: "wish"});
+    if (this.state.popularAnimes && this.state.popularAnimes.length > 0) {
+      const images = this.state.popularAnimes.map((anime) => {
+        return {
+          //: anime.user,
+          src: anime.imageLink,
+          //src: 'https://i.kinja-img.com/gawker-media/image/upload/s--s1IAfVS_--/c_fill,f_auto,fl_progressive,g_center,h_675,q_80,w_1200/kaprfadz9rnvypesa2u9.png',
+          thumbnail: anime.imageLink,
+          thumbnailWidth: 200,
+          thumbnailHeight: 300,
+          name: anime.name,
+          //likestatus:anime.likestatus,
+          //watchstatus: anime.watchstatus,
+          likestatus: 0,
+          watchstatus: 0,
+          animeID: anime.animeID,
+          //caption: post.message,
+        }
+      });
+      return <Gallery images={images} galleryType="Wish" animes={this.state.popularAnimes}
+                      loadAnimes={this.loadAnimes}
+                      loadFavoriteAnimes={this.loadFavoriteAnimes}
+                      loadRecommendAnimes = {this.loadRecommendAnimes}
+                      loadWishAnimes={this.loadWishAnimes}
+                      loadPopularAnimes={this.loadPopularAnimes}
+                      curTab = "popular"/>;
     }
     else {
       return null;
@@ -281,6 +320,27 @@ export class Home extends React.Component{
     });
   }
 
+  loadPopularAnimes = () =>{
+    const UserEmail = localStorage.getItem(TOKEN_KEY);
+    this.setState({loadPopularAnimes: true, error:''});
+    $.ajax({
+      url: `${API_ROOT}/wish?UserEmail=${UserEmail}`,
+      method: 'GET',
+      headers: {},
+    }).then((response)=>{
+        //this.setState({animes: dummy_animes, loadingAnimes: false, error: ''});
+        response = JSON.parse(response);
+        this.setState({popularAnimes: response || [], loadingWPopularAnimes: false, error: ''});
+        console.log(response);
+      }, (error) => {
+        this.setState({ loadingPopularAnimes: false, error: error.responseText });
+        console.log(error);
+      }
+    ).catch((error)=>{
+      console.log(error);
+    });
+  }
+
   loadSearchResAll = (value) =>{
     const UserEmail = localStorage.getItem(TOKEN_KEY);
     $.ajax({
@@ -357,6 +417,7 @@ export class Home extends React.Component{
     })
   }
 
+
   loadSearchDateResAll = (month, year) => {
     const UserEmail = localStorage.getItem(TOKEN_KEY);
     $.ajax({
@@ -417,6 +478,10 @@ export class Home extends React.Component{
           </TabPane>
           <TabPane tab="Wish List" key="4" curTab="wish">
             {this.getGalleryPanelContentWishAnimes()}
+          </TabPane>
+
+          <TabPane tab="Popular Animes" key="5" curTab="popular">
+            {this.getGalleryPanelContentPopularAnimes()}
           </TabPane>
         </Tabs>,
       </div>
