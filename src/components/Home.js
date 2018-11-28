63857957e24b652a7493c5ef1f416f096474e6fe
ZhaoruiDing <zhaorui1225@gmail.com>
2018-11-28 01:28:17 -0600

    final version 7

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ export class Home extends React.Component{
     favAnimes: [],
     recommendAnimes: [],
     wishAnimes: [],
-    popularAnimes[],
+    popularAnimes: [],
     error:'',
     loadingAnimes: false,
     loadingFavAnimes: false,
